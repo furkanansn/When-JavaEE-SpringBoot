b38b2987d4655eef73249679c637690ed849370e
hobedtech <info@hobedtech.com>
2020-12-10 20:23:34 +0300

    discover repository bug fixed

--- src/main/java/com/Hobedtech/when/dto/LoginRequest.java
+++ src/main/java/com/Hobedtech/when/dto/LoginRequest.java
@@ -2,13 +2,8 @@ package com.Hobedtech.when.dto;
 
 import lombok.Data;
 
-/**
- * Created by temelt on 15.02.2019.
- */
 @Data
 public class LoginRequest {
     private String username;
     private String password;
-    private String phone;
-
 }
