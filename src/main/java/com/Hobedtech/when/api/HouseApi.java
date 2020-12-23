a98a8d2e20c27cb8a1a6e3f7d4a301929afa2a67
hobedtech <info@hobedtech.com>
2020-12-23 04:19:12 +0300

    Security Update

--- src/main/java/com/Hobedtech/when/api/HouseApi.java
+++ src/main/java/com/Hobedtech/when/api/HouseApi.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,7 +38,7 @@ public class HouseApi {
         return ResponseEntity.ok(data);
     }
 
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("house/initialize")
     public ResponseEntity<List<HouseInitializeDto>> getHouseInitialize() {
         List<HouseInitializeDto> houseInitializeDto = eventServiceImpl.getHouseInitialize();
